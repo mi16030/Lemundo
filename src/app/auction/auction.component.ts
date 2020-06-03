7dbf33c504c99a237c5df7fd1f0d6183852d7446
mi16030 <tetejesandra@gmail.com>
2020-06-03 03:12:24 +0200

    dodata simulacija drugih licitatora i pobeda i poraz na aukciji

--- src/app/auction/auction.component.ts
+++ src/app/auction/auction.component.ts
@@ -17,6 +17,10 @@ export class AuctionComponent implements OnInit, OnDestroy {
   public aukcija: Aukcija;
   private paramMapSub: Subscription = null;
   private countdown = 59;
+  private aktuelna;
+  private najveca;
+  private nasa = false;
+  private brojPonuda;
 
   constructor(
     private route: ActivatedRoute,
@@ -32,7 +36,7 @@ export class AuctionComponent implements OnInit, OnDestroy {
 
       config.interval = 10000; // images change in 2sec //
 
-      config.wrap = true; // autometically redirect to first image //
+      config.wrap = true; // automatically redirect to first image //
 
       config.keyboard = false;
 
@@ -41,6 +45,9 @@ export class AuctionComponent implements OnInit, OnDestroy {
       this.auctionService.getAuctionsByCategoryAndId(kategorija, id).subscribe(
         (auction) => {
           this.aukcija = auction;
+          this.aktuelna = auction.aktuelnaPonuda;
+          this.najveca = auction.aktuelnaPonuda;
+          this.brojPonuda = auction.brojPonuda;
           console.log(this.aukcija);
         },
         (error) => console.log(error)
@@ -61,6 +68,20 @@ export class AuctionComponent implements OnInit, OnDestroy {
       this.paramMapSub.unsubscribe();
     }
   }
+
+  ponudjeno(): void {
+    let ponuda = parseInt(
+      (<HTMLInputElement>document.getElementById("nasaPonuda")).value,
+      10
+    );
+    if (ponuda > this.najveca) {
+      console.log("moja", ponuda);
+      if (!this.nasa) this.aktuelna = this.najveca + 10;
+      this.najveca = ponuda;
+      this.nasa = true;
+      this.brojPonuda++;
+    }
+  }
   newObservable4() {
     /*const interval$ = interval(1000);
 
@@ -75,7 +96,26 @@ export class AuctionComponent implements OnInit, OnDestroy {
     const countdownSubscription = countdown$.subscribe(
       (value) => {
         this.countdown--;
+        //let prob = Math.min((60 - this.countdown) / 120, 0.3);
+        let prob = 0.1;
+        let gen = Math.random();
+        if (gen < prob) {
+          let nova = Math.floor(
+            Math.random() * (this.aktuelna + 1000 - this.aktuelna + 1) +
+              this.aktuelna
+          );
+          console.log("njihova", nova);
+          if (nova > this.najveca) {
+            this.aktuelna = this.najveca + 10;
+            this.najveca = nova;
+            this.nasa = false;
+            this.brojPonuda++;
+          }
+        }
         if (this.countdown === 0) {
+          document.getElementById("vreme").innerText = this.nasa
+            ? "Pobedili ste"
+            : "Izgubili ste";
           countdownSubscription.unsubscribe();
         }
       },
